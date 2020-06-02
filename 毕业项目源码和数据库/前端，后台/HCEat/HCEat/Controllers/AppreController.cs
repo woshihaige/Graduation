fe6ab346ee7fb13347e16e71ed5fe1c1a98fc710
haigezhenshuai <1044334720@qq.com>
2020-06-02 13:43:02 +0800

    吃货鉴赏

---
+++ 毕业项目源码和数据库/前端，后台/HCEat/HCEat/Controllers/AppreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HCEat.Controllers
+{
+    public class AppreController : Controller
+    {
+        // GET: Appre
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
